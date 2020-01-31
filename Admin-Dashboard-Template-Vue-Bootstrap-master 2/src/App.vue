b727e72d911202cc87c1b9fa11aa8a38bab7c4db
nesara123 <mai-some-one@hotmail.com>
2020-01-31 16:41:07 +0700

    DF-trainee project BU

---
+++ Admin-Dashboard-Template-Vue-Bootstrap-master 2/src/App.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="flexible-content">
+    <!--Navbar-->
+    <mdb-navbar class="flexible-navbar white" light position="top" scrolling>
+      <mdb-navbar-brand target="_blank"><img alt="" class="img-fluid" src="./assets/D-Trainee.png"/></mdb-navbar-brand>
+      <mdb-navbar-toggler>
+        <mdb-navbar-nav right>
+          <mdb-nav-item href="#!" waves-fixed><mdb-icon icon="search"/></mdb-nav-item>
+          <mdb-nav-item href="#!" waves-fixed><mdb-icon icon="bell"/></mdb-nav-item>
+          <mdb-nav-item href="#!" waves-fixed><mdb-icon icon="user-alt"/></mdb-nav-item>
+        </mdb-navbar-nav>
+      </mdb-navbar-toggler>
+    </mdb-navbar>
+    <!--/.Navbar-->
+    <!-- Sidebar -->
+    <div class="sidebar-fixed position-fixed">
+      <a class="logo-wrapper"><img alt="" class="img-fluid" src="./assets/1_w55hpZI8EkcHxhghZjK-NA 1.png"/></a>
+      <mdb-list-group class="list-group-flush">
+        <router-link to="/home" @click.native="activeItem = 1">
+          <mdb-list-group-item :action="true" :class="activeItem === 1 && 'active'"><mdb-icon icon="home" class="mr-3"/>หน้าหลัก</mdb-list-group-item>
+        </router-link>
+        <router-link to="/calendar" @click.native="activeItem = 2">
+          <mdb-list-group-item :action="true" :class="activeItem === 2 && 'active'"><mdb-icon icon="calendar-alt" class="mr-3"/>ปฏิทิน</mdb-list-group-item>
+        </router-link>
+        <router-link to="/contacts" @click.native="activeItem = 3">
+          <mdb-list-group-item :action="true" :class="activeItem === 3 && 'active'"><mdb-icon icon="address-book" class="mr-3"/>ผู้ติดต่อ</mdb-list-group-item>
+        </router-link>
+        <router-link to="/task" @click.native="activeItem = 4">
+          <mdb-list-group-item :action="true" :class="activeItem === 4 && 'active'"><mdb-icon icon="calendar-check" class="mr-3"/>ทาร์คงาน</mdb-list-group-item>
+        </router-link>
+        <router-link to="/profile" @click.native="activeItem = 5">
+          <mdb-list-group-item :action="true" :class="activeItem === 5 && 'active'"><mdb-icon icon="user" class="mr-3"/>โปร์ไฟล์</mdb-list-group-item>
+        </router-link>
+      </mdb-list-group>
+    </div>
+    <!-- /Sidebar  -->
+    <main>
+      <div class="mt-5 p-5">
+        <router-view/>
+      </div>
+      <ftr color="primary-color-dark" class="text-center font-small darken-2">
+        <hr class="my4"/>
+        <div class="pb-4">
+          <a href="#"><mdb-icon fab icon="facebook-square" class="mr-3"/></a>
+          <a href="#"><mdb-icon fab icon="twitter" class="mr-3"/></a>
+          <a href="#"><mdb-icon fab icon="youtube" class="mr-3"/></a>
+          <a href="#"><mdb-icon fab icon="google-plus" class="mr-3"/></a>
+          <a href="#"><mdb-icon fab icon="dribbble" class="mr-3"/></a>
+          <a href="#"><mdb-icon fab icon="pinterest" class="mr-3"/></a>
+          <a href="#"><mdb-icon fab icon="github" class="mr-3"/></a>
+          <a href="#"><mdb-icon fab icon="codepen" class="mr-3"/></a>
+        </div>
+        <p class="footer-copyright mb-0 py-3 text-center">
+          &copy; {{new Date().getFullYear()}} Copyright:
+        </p>
+      </ftr>
+    </main>
+  </div>
+</template>
+
+<script>
+import { mdbContainer, mdbNavbar, mdbNavbarBrand, mdbNavItem, mdbNavbarNav, mdbNavbarToggler, mdbBtn, mdbIcon, mdbListGroup, mdbListGroupItem, mdbCardBody, mdbFooter, waves } from 'mdbvue'
+import Dashboard from './components/Dashboard'
+import Profile from './components/Profile'
+
+export default {
+  name: 'AdminTemplate',
+  components: {
+    mdbContainer,
+    mdbNavbar,
+    mdbNavbarBrand,
+    mdbNavItem,
+    mdbNavbarNav,
+    mdbNavbarToggler,
+    mdbBtn,
+    mdbListGroup,
+    mdbListGroupItem,
+    mdbIcon,
+    mdbCardBody,
+    'ftr': mdbFooter,
+    Dashboard,
+    Profile
+  },
+  data () {
+    return {
+      activeItem: 1
+    }
+  },
+  beforeMount () {
+    this.activeItem = this.$route.matched[0].props.default.page
+  },
+  mixins: [waves]
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+main {
+  background-color: #EDEDEE;
+}
+
+.flexible-content {
+  transition: padding-left 0.3s;
+  padding-left: 270px;
+}
+
+.flexible-navbar {
+  transition: padding-left 0.5s;
+  padding-left: 270px;
+}
+
+.sidebar-fixed {
+  left: 0;
+  top: 0;
+  height: 100vh;
+  width: 270px;
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  z-index: 1050;
+  background-color: #293241;
+  padding: 1.5rem;
+  padding-top: 0;
+}
+.img-fluid {
+  width: 100px;
+}
+
+.sidebar-fixed .logo-wrapper img{
+  width: 100%;
+  padding: 2.5rem;
+}
+
+.sidebar-fixed .list-group-item {
+  display: block !important;
+  transition: background-color 0.3s;
+}
+
+.sidebar-fixed .list-group .active {
+  box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12);
+  border-radius: 5px;
+}
+
+@media (max-width: 1199.98px) {
+  .sidebar-fixed {
+    display: none;
+  }
+  .flexible-content {
+    padding-left: 0;
+  }
+  .flexible-navbar {
+    padding-left: 10px;
+  }
+}
+</style>
+<style>
+.navbar-light .navbar-brand {
+  margin-left: 15px;
+  color: #2196f3 !important;
+  font-weight: bolder;
+}
+</style>
---
+++ Admin-Dashboard-Template-Vue-Bootstrap-master 2/src/view/Task.vue
@@ -0,0 +1,247 @@
+<template>
+  <section id="task">
+    <mdb-row>
+      <mdb-col>
+        <h1>Task</h1>
+      </mdb-col>
+      <mdb-col></mdb-col>
+      <mdb-col></mdb-col>
+      <mdb-col col="2">
+        <mdb-btn @click.native="showFluidModalRight = true" class="mb-0">สร้าง</mdb-btn>
+      </mdb-col>
+    </mdb-row>
+    <mdb-card cascade narrow class="mt-2">
+      <mdb-card-body class="text-center">
+        <mdb-row>
+          <mdb-col>
+            <div id="task-btn">
+              <mdb-btn-group vertical style="width: 100%">
+                <mdb-btn @click.native="showFluidModalRight = true" class="mb-0" color="amber">
+                  <div style="text-align: start"><mdb-icon icon="bookmark" /></div>
+                </mdb-btn>
+                <mdb-btn @click.native="showFluidModalRight = true" class="mb-0" color="amber">
+                  <div style="text-align: start"><mdb-icon icon="bookmark" /></div>
+                </mdb-btn>
+                <mdb-btn @click.native="showFluidModalRight = true" class="mb-0" color="amber">
+                  <div style="text-align: start"><mdb-icon icon="bookmark" /></div>
+                </mdb-btn>
+                <mdb-btn @click.native="showFluidModalRight = true" class="mb-0" color="amber">
+                  <div style="text-align: start"><mdb-icon icon="bookmark" /></div>
+                </mdb-btn>
+                <mdb-btn @click.native="showFluidModalRight = true" class="mb-0" color="amber">
+                  <div style="text-align: start"><mdb-icon icon="bookmark" /></div>
+                </mdb-btn>
+                <mdb-btn @click.native="showFluidModalRight = true" class="mb-0" color="amber">
+                  <div style="text-align: start"><mdb-icon icon="bookmark" /></div>
+                </mdb-btn>
+                <mdb-btn @click.native="showFluidModalRight = true" class="mb-0" color="amber">
+                  <div style="text-align: start"><mdb-icon icon="bookmark" /></div>
+                </mdb-btn>
+                <mdb-btn @click.native="showFluidModalRight = true" class="mb-0" color="amber">
+                  <div style="text-align: start"><mdb-icon icon="bookmark" /></div>
+                </mdb-btn>
+                <mdb-btn @click.native="showFluidModalRight = true" class="mb-0" color="amber">
+                  <div style="text-align: start"><mdb-icon icon="bookmark" /></div>
+                </mdb-btn>
+                <mdb-btn @click.native="showFluidModalRight = true" class="mb-0" color="amber">
+                  <div style="text-align: start"><mdb-icon icon="bookmark" /></div>
+                </mdb-btn>
+              </mdb-btn-group>
+            </div>
+            <br>
+          </mdb-col>
+        </mdb-row>
+        <mdb-row>
+          <mdb-col></mdb-col>
+          <mdb-col><mdb-pagination circle>
+            <mdb-page-item disabled>First</mdb-page-item>
+            <mdb-page-nav prev disabled></mdb-page-nav>
+            <mdb-page-item active>1</mdb-page-item>
+            <mdb-page-item>2</mdb-page-item>
+            <mdb-page-item>3</mdb-page-item>
+            <mdb-page-item>4</mdb-page-item>
+            <mdb-page-item>5</mdb-page-item>
+            <mdb-page-nav next></mdb-page-nav>
+            <mdb-page-item>Last</mdb-page-item>
+          </mdb-pagination></mdb-col>
+          <mdb-col></mdb-col>
+        </mdb-row>
+        <!------ Modal Form ------>
+        <mdb-modal fullHeight position="right" :show="showModalCreate" @close="showModalCreate = false">
+          <mdb-modal-header>
+            <mdb-modal-title>showModalCreate</mdb-modal-title>
+          </mdb-modal-header>
+          <mdb-modal-body>...</mdb-modal-body>
+          <mdb-modal-footer>
+            <mdb-btn color="secondary" @click.native="showModalCreate = false">Close</mdb-btn>
+            <mdb-btn color="primary">Save changes</mdb-btn>
+          </mdb-modal-footer>
+        </mdb-modal>
+        <!---->
+        <!------ Modal ------>
+        <mdb-modal fullHeight position="right" :show="showFluidModalRight" @close="showFluidModalRight = false">
+          <mdb-modal-header>
+            <mdb-modal-title>Modal title</mdb-modal-title>
+          </mdb-modal-header>
+          <mdb-modal-body>...</mdb-modal-body>
+          <mdb-modal-footer>
+            <mdb-btn color="secondary" @click.native="showFluidModalRight = false">Close</mdb-btn>
+            <mdb-btn color="primary">Save changes</mdb-btn>
+          </mdb-modal-footer>
+        </mdb-modal>
+        <!---->
+      </mdb-card-body>
+    </mdb-card>
+  </section>
+</template>
+
+<script>
+import {
+  mdbRow,
+  mdbCol,
+  mdbCard,
+  mdbView,
+  mdbCardBody,
+  mdbBtn,
+  mdbCardHeader,
+  mdbCardText,
+  mdbIcon,
+  mdbTbl,
+  mdbBarChart,
+  mdbPieChart,
+  mdbLineChart,
+  mdbRadarChart,
+  mdbDoughnutChart,
+  mdbListGroup,
+  mdbListGroupItem,
+  mdbBadge,
+  mdbModal,
+  mdbModalHeader,
+  mdbModalTitle,
+  mdbModalBody,
+  mdbModalFooter,
+  mdbBtnGroup, mdbDropdown, mdbDropdownItem, mdbDropdownMenu, mdbDropdownToggle, mdbPagination,
+  mdbPageItem,
+  mdbPageNav
+} from 'mdbvue'
+
+export default {
+  name: 'Task',
+  components: {
+    mdbRow,
+    mdbCol,
+    mdbCard,
+    mdbView,
+    mdbCardBody,
+    mdbBtn,
+    mdbCardHeader,
+    mdbCardText,
+    mdbIcon,
+    mdbTbl,
+    mdbBarChart,
+    mdbPieChart,
+    mdbLineChart,
+    mdbRadarChart,
+    mdbDoughnutChart,
+    mdbListGroup,
+    mdbListGroupItem,
+    mdbBadge,
+    mdbModal,
+    mdbModalHeader,
+    mdbModalTitle,
+    mdbModalBody,
+    mdbModalFooter,
+    mdbBtnGroup,
+    mdbDropdown,
+    mdbDropdownItem,
+    mdbDropdownMenu,
+    mdbDropdownToggle,
+    mdbPagination,
+    mdbPageItem,
+    mdbPageNav
+  },
+  data () {
+    return {
+      showFrameModalTop: false,
+      showFrameModalBottom: false,
+      showSideModalTopRight: false,
+      showSideModalTopLeft: false,
+      showSideModalBottomRight: false,
+      showSideModalBottomLeft: false,
+      showCentralModalSmall: false,
+      showCentralModalMedium: false,
+      showCentralModalLarge: false,
+      showCentralModalFluid: false,
+      showFluidModalRight: false,
+      showFluidModalLeft: false,
+      showFluidModalTop: false,
+      showFluidModalBottom: false,
+      showModalCreate: false,
+      barChartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            barPercentage: 1,
+            gridLines: {
+              display: true,
+              color: 'rgba(0, 0, 0, 0.1)'
+            }
+          }],
+          yAxes: [{
+            gridLines: {
+              display: true,
+              color: 'rgba(0, 0, 0, 0.1)'
+            },
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .cascading-admin-card .admin-up {
+    margin-left: 4%;
+    margin-right: 4%;
+    margin-top: -20px;
+  }
+  .cascading-admin-card .admin-up .fas,
+  .cascading-admin-card .admin-up .far {
+    box-shadow: 0 2px 9px 0 rgba(0, 0, 0, 0.2), 0 2px 13px 0 rgba(0, 0, 0, 0.19);
+    padding: 1.7rem;
+    font-size: 2rem;
+    color: #fff;
+    text-align: left;
+    margin-right: 1rem;
+    border-radius: 3px;
+  }
+  .cascading-admin-card .admin-up .data {
+    float: right;
+    margin-top: 2rem;
+    text-align: right;
+  }
+  .admin-up .data p {
+    color: #999999;
+    font-size: 12px;
+  }
+  .classic-admin-card .card-body {
+    color: #fff;
+    margin-bottom: 0;
+    padding: 0.9rem;
+  }
+  .classic-admin-card .card-body p {
+    font-size: 13px;
+    opacity: 0.7;
+    margin-bottom: 0;
+  }
+  .classic-admin-card .card-body h4 {
+    margin-top: 10px;
+  }
+</style>
